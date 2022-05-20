c70320a9fbae39b3cecd326aff4e0fead3499160
Niraj Bhattarai <nirajbhattarai19@gmail.com>
2022-05-20 11:26:12 -0400

    changed to bsc

--- handler.js
+++ handler.js
@@ -1,47 +1,38 @@
-'use strict';
-
-const Caver = require('caver-js');
-const WhiteListController = require('./contracts/WhiteListController');
-
-const caver = new Caver(
-  new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", {
-    headers: [
-      {
-        name: "Authorization",
-        value: "Basic S0FTS0ZDRzJDTzVYTEdSS1RaQzVaM1FEOnVLVThZa0w5ZF9lQ09vZ2hQejVkamZBeXpIS0M3WmhURGZ1OHBuclo=",
-      },
-      // 8217 is mainnet's chain-id
-      // 1001 is testnet's chain-id
-      { name: "x-chain-id", value: process.env.mainnet ? 8217 : 1001 },
-    ],
-    keepAlive: true,
-  })
-);
+"use strict";
+const Web3 = require("web3");
+const WhiteListController = require("./contracts/WhiteListController");
 
 //All variable will be pulled from env once deployed sls
 const transferController = process.env.controllerAddress;
 const publicAddress = process.env.publicAddress;
 const privateKey = process.env.privateKey;
 
-const controller = new caver.klay.Contract(WhiteListController.abi, transferController).methods;
+const RPCURL_BSC_MAINNET = "https://bsc-dataseed.binance.org/";
+
+const web3 = new Web3(RPCURL_BSC_MAINNET);
+
+const controller = new web3.eth.Contract(
+  WhiteListController.abi,
+  transferController
+).methods;
+
 module.exports.whitelist = async (event) => {
   console.log(event);
-  if (!event.hasOwnProperty('queryStringParameters')) {
+  if (!event.hasOwnProperty("queryStringParameters")) {
     return {
       statusCode: 400,
       body: JSON.stringify({
-        message: 'Invalid address Provided'
+        message: "Invalid address Provided",
       }),
     };
-
   }
   const address = event.queryStringParameters.address;
-  console.log(address)
-  if (!caver.utils.isAddress(address)) {
+  console.log(address);
+  if (!web3.utils.isAddress(address)) {
     return {
       statusCode: 400,
       body: JSON.stringify({
-        message: 'Invalid address Provided'
+        message: "Invalid address Provided",
       }),
     };
   }
@@ -53,7 +44,7 @@ module.exports.whitelist = async (event) => {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: 'Address Already Whitelisted'
+          message: "Address Already Whitelisted",
         },
         null,
         2
@@ -63,31 +54,31 @@ module.exports.whitelist = async (event) => {
 
   let data = controller.addOrChangeUserStatus(address, true).encodeABI();
 
-  let signedTransaction = await caver.klay.accounts.signTransaction(
+  const signedTransaction = await web3.eth.accounts.signTransaction(
     {
-      type: "SMART_CONTRACT_EXECUTION",
       from: publicAddress,
       to: transferController,
       data,
-      gas: 305026,
+      gas: "53484",
     },
     privateKey
   );
 
   try {
-    const { transactionHash } = await caver.klay.sendSignedTransaction(signedTransaction.rawTransaction);
+    const { transactionHash } = await web3.eth.sendSignedTransaction(
+      signedTransaction.rawTransaction
+    );
     return {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: 'Address Whitelisted Successfully',
+          message: "Address Whitelisted Successfully",
           transactionHash,
         },
         null,
         2
       ),
     };
-
   } catch (err) {
     console.log(err);
 
@@ -95,7 +86,7 @@ module.exports.whitelist = async (event) => {
       statusCode: 503,
       body: JSON.stringify(
         {
-          message: 'Something Went Wrong. Please Contact To Customer Care',
+          message: "Something Went Wrong. Please Contact To Customer Care",
         },
         null,
         2
